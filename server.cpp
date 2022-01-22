1ff33a7edd9cbf360941ef888b21018878e4ea94
plaskod <davidblackfyre1@gmail.com>
2022-01-22 11:58:17 +0100

    epoll server

--- server.cpp
+++ server.cpp
@@ -1,28 +1,22 @@
-/* 
-Guide to network programming: https://beej.us/guide/bgnet/html/
-*/
-
-
-#include <iostream>
-#include <stdio.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
+#include <cstdlib>
+#include <cstdio>
 #include <unistd.h>
-#include <string.h>
-
-#include <sys/socket.h>	
+#include <sys/socket.h>
+#include <netinet/in.h>
 #include <arpa/inet.h>
 #include <errno.h>
-#include <netinet/in.h>
-#include <string>
+#include <error.h>
+#include <netdb.h>
+#include <sys/epoll.h>
+#include <unordered_set>
+#include <signal.h>
 #include <vector>
-#include <unordered_map>
 #include <algorithm>
-#include "error.h"
+#include <iostream>
+#include <cstring>
 
+using namespace std;
 
-#define PORT 8080
-#define MAX_CLIENTS 2 // to be changed to an arbitrary number
 
 std::vector<std::string> usernames;
 // std::unordered_map<int, User> users;
@@ -33,11 +27,11 @@ constexpr char words[3][20] = {
     "protocol"
 };
 
-struct Client{
-    std::string username;
-    int remaining_lives = 9;
-    int socket;
-} clients[MAX_CLIENTS];
+// struct Client{
+//     std::string username;
+//     int remaining_lives = 9;
+//     int socket;
+// } clientss;
 
 struct HangmanGame{
     bool is_on = 0;
@@ -47,14 +41,10 @@ struct HangmanGame{
     char word_to_guess[20];
 } game;
 
-class Player{
-    private:
-        //socket
-        int socket;
+struct Player{
+    std::string username = "";
+    int remaining_lives = 2;
 
-    public:
-        std::string username;
-        int remaining_lives;
 };
 
 
@@ -66,31 +56,175 @@ bool check_username(std::string name){
     return false;
 }
 
-constexpr const int one = 1;
+
+class Client;
+
+int servFd;
+int epollFd;
+std::unordered_set<Client*> clients;
+
+std::unordered_set<Player*> players;
+
+void ctrl_c(int);
+
+void sendToAllBut(int fd, char * buffer, int count);
+
+uint16_t readPort(char * txt);
+
+void setReuseAddr(int sock);
+
+struct Handler {
+    virtual ~Handler(){}
+    virtual void handleEvent(uint32_t events) = 0;
+};
+
+class Client : public Handler {
+    int _fd;
+    std::string username;
+    int remaining_lives = 2;
+
+
+public:
+    Client(int fd) : _fd(fd) {
+
+        // players.insert();
+        epoll_event ee {EPOLLIN|EPOLLRDHUP, {.ptr=this}};
+        epoll_ctl(epollFd, EPOLL_CTL_ADD, _fd, &ee);
+    }
+    virtual ~Client(){
+        epoll_ctl(epollFd, EPOLL_CTL_DEL, _fd, nullptr);
+        shutdown(_fd, SHUT_RDWR);
+        close(_fd);
+    }
+    int fd() const {return _fd;}
+
+
+    virtual void handleEvent(uint32_t events) override {
+        if(events & EPOLLIN) {
+            char buffer[256];
+            ssize_t count = read(_fd, buffer, 256);
+            if(count > 0){
+                std::cout<<buffer;
+                if(buffer[0] == '1'){
+                    char s[] = "Podaj nazwe: ";
+                    write(s, strlen(s));
+
+                    int stan = 0;
+                    set_username(s);
+                    cout << "FUnkcja set_nickname\n";
+                    cout << username;
+
+                }
+
+
+
+                sendToAllBut(_fd, buffer, count); // chat - wyslij kazdemu co wyslal klient
+            }
+                
+            else
+                events |= EPOLLERR;
+        }
+        if(events & ~EPOLLIN){
+            remove();
+        }
+    }
+    void write(char * buffer, int count){
+        if(count != ::write(_fd, buffer, count))
+            remove();
+        
+    }
+    void remove() {
+        printf("removing %d\n", _fd);
+        clients.erase(this);
+        delete this;
+    }
+
+    void set_username(std::string nick){
+        username = nick;
+    }
+};
+
+class : Handler {
+    public:
+    virtual void handleEvent(uint32_t events) override {
+        if(events & EPOLLIN){
+            sockaddr_in clientAddr{};
+            socklen_t clientAddrSize = sizeof(clientAddr);
+            
+            auto clientFd = accept(servFd, (sockaddr*) &clientAddr, &clientAddrSize);
+            if(clientFd == -1) error(1, errno, "accept failed");
+            
+            printf("new connection from: %s:%hu (fd: %d)\n", inet_ntoa(clientAddr.sin_addr), ntohs(clientAddr.sin_port), clientFd);
+            
+            clients.insert(new Client(clientFd));
+        }
+        if(events & ~EPOLLIN){
+            error(0, errno, "Event %x on server socket", events);
+            ctrl_c(SIGINT);
+        }
+    }
+} servHandler;
 
 int main(int argc, char ** argv){
-    printf("Starting server...\n");
-    sockaddr_in serverAddress{
-        .sin_family = AF_INET,
-        .sin_port   = htons(PORT),
-        .sin_addr   = {htonl(INADDR_ANY)}
-    };
-
-    int serverSock;
-    if(serverSock = socket(AF_INET, SOCK_STREAM, 0) < 0)
-        error(1, errno, "Creating socket failed!");
-
-    setsockopt(serverSock, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(one));
-
-    if(bind(serverSock, (sockaddr*) &serverAddress, sizeof(serverAddress)) < 0)
-        error(1,errno,"Bind failed!");
+    if(argc != 2) error(1, 0, "Need 1 arg (port)");
+    auto port = readPort(argv[1]);
     
+    servFd = socket(AF_INET, SOCK_STREAM, 0);
+    if(servFd == -1) error(1, errno, "socket failed");
+    
+    signal(SIGINT, ctrl_c);
+    signal(SIGPIPE, SIG_IGN);
+    
+    setReuseAddr(servFd);
+    
+    sockaddr_in serverAddr{.sin_family=AF_INET, .sin_port=htons((short)port), .sin_addr={INADDR_ANY}};
+    int res = bind(servFd, (sockaddr*) &serverAddr, sizeof(serverAddr));
+    if(res) error(1, errno, "bind failed");
+    
+    res = listen(servFd, 1);
+    if(res) error(1, errno, "listen failed");
 
-    if(listen(serverSock, 1) < 0)
-        error(1,errno,"Listen failed!");
+    epollFd = epoll_create1(0);
+    
+    epoll_event ee {EPOLLIN, {.ptr=&servHandler}};
+    epoll_ctl(epollFd, EPOLL_CTL_ADD, servFd, &ee);
+    
+    while(true){
+        if(-1 == epoll_wait(epollFd, &ee, 1, -1)) {
+            error(0,errno,"epoll_wait failed");
+            ctrl_c(SIGINT);
+        }
+        ((Handler*)ee.data.ptr)->handleEvent(ee.events);
+    }
+}
 
-    while(true) {
-        accept(serverSock, nullptr, nullptr);
-        printf("Accepted a new connection, ignoring it.\n");
+uint16_t readPort(char * txt){
+    char * ptr;
+    auto port = strtol(txt, &ptr, 10);
+    if(*ptr!=0 || port<1 || (port>((1<<16)-1))) error(1,0,"illegal argument %s", txt);
+    return port;
+}
+
+void setReuseAddr(int sock){
+    const int one = 1;
+    int res = setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(one));
+    if(res) error(1,errno, "setsockopt failed");
+}
+
+void ctrl_c(int){
+    for(Client * client : clients)
+        delete client;
+    close(servFd);
+    printf("Closing server\n");
+    exit(0);
+}
+
+void sendToAllBut(int fd, char * buffer, int count){
+    auto it = clients.begin();
+    while(it!=clients.end()){
+        Client * client = *it;
+        it++;
+        if(client->fd()!=fd)
+            client->write(buffer, count);
     }
 }
